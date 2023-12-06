ad9fb0fa8091770b82bcf44822ac47d5e4640d34
System Administrator <root@abcs-MacBook-Pro.local>
2023-12-06 23:30:43 +0530

    some changes

--- app/leaderboard/page.jsx
+++ app/leaderboard/page.jsx
@@ -6,24 +6,24 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/player/[...slug]/route";
 
 const Page = async () => {
-  
-  const response = await fetch(`${process.env.API_BASE_URL}/api/top-players`);
 
-  if (!response.ok) {
-    const error = new Error(`Error fetching data: ${response.status}`);
-    throw error;
-  }
-  const data = await response.json();
+  // const response = await fetch(`${process.env.API_BASE_URL}/api/top-players`);
 
-  if (data) {
-    return (
-      <>
-        <LeaderBoard res={data} />;
-      </>
-    );
-  } else {
+  // if (!response.ok) {
+  //   const error = new Error(`Error fetching data: ${response.status}`);
+  //   throw error;
+  // }
+  // const data = await response.json();
+
+  // if (data) {
+  //   return (
+  //     <>
+  //       <LeaderBoard res={data} />;
+  //     </>
+  //   );
+  // } else {
     return <div>Sorry Something Went wrong</div>;
-  }
+  // }
 };
 
 export default Page;
--- app/player/[...slug]/page.jsx
+++ app/player/[...slug]/page.jsx
@@ -5,32 +5,32 @@ import { authOptions } from "../../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 
 const Page = async ({ params }) => {
-  const session = await getServerSession(authOptions)
-  if (!session){
-    redirect("/api/auth/signin")
-  }
+  // const session = await getServerSession(authOptions)
+  // if (!session){
+  //   redirect("/api/auth/signin")
+  // }
   
-  const { slug } = params;
-  const playerId = slug
-  const response = await fetch(
-    `${process.env.API_BASE_URL}/api/player/${playerId}/rating-history`
-  );
+  // const { slug } = params;
+  // const playerId = slug
+  // const response = await fetch(
+  //   `${process.env.API_BASE_URL}/api/player/${playerId}/rating-history`
+  // );
   
 
-  if (!response.ok) {
-    const error = new Error(`Error fetching data: ${response.status}`);
-    throw error;
-  }
-  const data = await response.json();
-  console.log(data)
-  if(!data){
+  // if (!response.ok) {
+  //   const error = new Error(`Error fetching data: ${response.status}`);
+  //   throw error;
+  // }
+  // const data = await response.json();
+  // console.log(data)
+  // if(!data){
     return(
       <>
       <h1>Opps something went wrong</h1>
       </>
     )
-  }
-  return <UserProfile res={data} />;
+  // }
+  // return <UserProfile res={data} />;
 };
 
 export default Page;
